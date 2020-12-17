7e80c5932b46984964221f8f85bb892ffeed6183
XeonWangYue <qiwang18@qq.com>
2020-12-17 19:47:38 +0800

    新建售后订单

--- src/main/java/cn/edu/xmu/ooad/other/required/model/OtherOrderSend.java
+++ src/main/java/cn/edu/xmu/ooad/other/required/model/OtherOrderSend.java
@@ -1,10 +1,14 @@
 package cn.edu.xmu.ooad.other.required.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OtherOrderSend {
 	Long skuId;
 	Long shopId;
@@ -13,4 +17,5 @@ public class OtherOrderSend {
 	Integer quantity;
 	String address;
 	String mobile;
+	Long customerId;
 }
