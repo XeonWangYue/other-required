22d1c7a697baf8648c4fdfa7ac0f95c6e77d4c82
XeonWangYue <qiwang18@qq.com>
2020-12-21 15:42:30 +0800

    psuh

--- src/main/java/cn/edu/xmu/ooad/other/required/model/OtherOrderSend.java
+++ src/main/java/cn/edu/xmu/ooad/other/required/model/OtherOrderSend.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OtherOrderSend {
-	Long skuId;
+	Long orderItemId;
 	Long shopId;
 	Long regionId;
 	String consignee;
